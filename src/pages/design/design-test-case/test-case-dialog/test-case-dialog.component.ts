527f09e3b8ea692bb4f7a6064e19344ba8c6a316
Agastya <ritesh.bawaskar@gmail.com>
2020-09-07 12:24:43 +0530

    bug fix

--- src/pages/design/design-test-case/test-case-dialog/test-case-dialog.component.ts
+++ src/pages/design/design-test-case/test-case-dialog/test-case-dialog.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { Component, OnInit , Inject} from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import {MatDialogRef} from '@angular/material/dialog';
+import {MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-test-case-dialog',
@@ -7,9 +11,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestCaseDialogComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialogRef: MatDialogRef<TestCaseDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) { }
 
   ngOnInit() {
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 
 }
